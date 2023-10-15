906b0d657585ab847896a58ef888be3a6abc7f9e
Caleb Hunt <caleb.hunt.j@gmail.com>
2023-10-14 23:29:31 -0600

    Send requests concurrently
    Next up: Clean up naming and whatnot. So ugly.

--- main.go
+++ main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"agility-take-home/models"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
-	"text/template"
-
 	"slices"
+	"strings"
+	"sync"
+	"text/template"
 )
 
 const (
@@ -31,24 +32,44 @@ func main() {
 
 	people := searchResult.Results
 	finals := make([]models.Final, len(people)) // Ahhh yeahh gotta save those allocations
+	wg := sync.WaitGroup{}
 	for i, p := range people {
-		finals[i].Person = p
-		starships, err := getFromUrls[models.Starship](p.StarshipUrls)
-		if err != nil {
-			panic(err)
-		}
-		finals[i].Starships = starships
-		homeplanet, err := getFromUrl[models.Planet](p.HomeworldUrl)
-		if err != nil {
-			panic(err)
-		}
-		finals[i].HomePlanet = homeplanet
-		species, err := getFromUrls[models.Species](p.SpeciesUrls)
-		if err != nil {
-			panic(err)
-		}
-		finals[i].Species = species
+		wg.Add(1)
+		go func(i int, p models.Person) {
+			defer wg.Done()
+			wg1 := sync.WaitGroup{}
+			finals[i].Person = p
+			wg1.Add(1)
+			go func() {
+				defer wg1.Done()
+				starships, err := getFromUrls[models.Starship](p.StarshipUrls)
+				if err != nil {
+					panic(err)
+				}
+				finals[i].Starships = starships
+			}()
+			wg1.Add(1)
+			go func() {
+				defer wg1.Done()
+				homeplanet, err := getFromUrl[models.Planet](p.HomeworldUrl)
+				if err != nil {
+					panic(err)
+				}
+				finals[i].HomePlanet = homeplanet
+			}()
+			wg1.Add(1)
+			go func() {
+				defer wg1.Done()
+				species, err := getFromUrls[models.Species](p.SpeciesUrls)
+				if err != nil {
+					panic(err)
+				}
+				finals[i].Species = species
+			}()
+			wg1.Wait()
+		}(i, p)
 	}
+	wg.Wait()
 
 	slices.SortFunc(finals, func(a, b models.Final) int { return strings.Compare(a.Person.Name, b.Person.Name) })
 
@@ -61,16 +82,30 @@ func main() {
 }
 
 func getFromUrls[T any](urls []string) ([]T, error) {
-	starships := make([]T, len(urls))
+	ts := make([]T, len(urls))
+	// errChan acts as our wait group
+	errChan := make(chan error)
+	defer close(errChan)
 	for i, url := range urls {
-		starship, err := getFromUrl[T](url)
-		if err != nil {
-			return nil, err
-		}
+		// Get each url concurrently
+		go func(i int, url string) {
+			t, err := getFromUrl[T](url)
+			if err != nil {
+				errChan <- err
+				return
+			}
 
-		starships[i] = starship
+			ts[i] = t
+			errChan <- nil
+		}(i, url)
 	}
-	return starships, nil
+	var retErr error = nil
+	for i := 0; i < len(urls); i++ {
+		if err := <-errChan; err != nil {
+			retErr = errors.Join(err, retErr)
+		}
+	}
+	return ts, nil
 }
 
 func getFromUrl[T any](url string) (T, error) {
